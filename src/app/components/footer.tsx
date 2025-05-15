24a9a9b127b27c73eeaf722eae99026844735d23
waniyah_khan <khanwaniyah321@GMAIL.COM>
2025-05-15 14:09:52 +0500

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,18 +3,22 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-        <Link href="/" className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-          <img src="\assets\image.png" alt="Logo" className="h-10 w-auto mr-3 rounded-full"  /> {/* Adjust the height and width as needed */}
-         
+    <footer className="bg-gradient-to-r from-blue-700 to-purple-800 text-white">
+      <div className="container px-5 py-2 mx-auto flex flex-col sm:flex-row items-center justify-between">
+        {/* Logo */}
+        <Link href="/" className="flex items-center mb-2 sm:mb-0">
+          <img
+            src="/assets/alphalogo.png"
+            alt="Logo"
+            className="h-12 w-auto mr-3"
+          />
         </Link>
-          <span className="ml-3 text-xl">AlphaInsights</span>
-        </a>
-        <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
-        Empowering Success Across Education, Business, and Beyond</p>
-     </div>
+
+        {/* Slogan */}
+        <p className="text-xs sm:text-sm text-white text-center sm:text-right max-w-md">
+          Empowering Success Across Education, Business, and Beyond
+        </p>
+      </div>
     </footer>
   );
 };
